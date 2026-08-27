b4f8c90e626b9b543c006fa8c48068479b67ad0d
triglan <glanb4277@gmail.com>
2026-08-27 15:52:05 +0900

    feat: 2번째 다른 출력

--- src/main/java/com/test/NewJava2.java
+++ src/main/java/com/test/NewJava2.java
@@ -4,5 +4,12 @@ public class NewJava2 {
     public static void main(String[] args) {
         System.out.println("다른 출력");
         System.out.println("2번째 다른 출력");
+        System.out.println("2번째 다른 출력");
+        System.out.println("2번째 다른 출력");
+        System.out.println("2번째 다른 출력");
+        System.out.println("2번째 다른 출력");
+        System.out.println("2번째 다른 출력");
+        System.out.println("2번째 다른 출력");
+        System.out.println("2번째 다른 출력");
     }
 }
