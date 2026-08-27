6f28f4cdb672c7fbba9d673c7c563c047c2334f2
triglan <glanb4277@gmail.com>
2026-08-27 15:06:03 +0900

    feat: 새로운 자바의 꿈

--- src/main/java/com/test/NewJava.java
+++ src/main/java/com/test/NewJava.java
@@ -4,5 +4,6 @@ public class NewJava {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("새로운 자바");
+        System.out.println("새로운 자바의 꿈");
     }
 }
